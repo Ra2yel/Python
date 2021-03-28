3ecd6c8a492fcef6732f7001fe04df4681607b19
Ra2yel <jagielakj@gmail.com>
2021-03-28 10:42:26 +0200

    Add files via upload

---
+++ Papadam oeiizk konkurs informatyczny 2021.py
@@ -0,0 +1,16 @@
+def papadam(wyraz):
+    temp = 'papadam'
+    count = 0
+    wynik = 0
+    while len(wyraz) != 0:
+        if wyraz[0] == temp[0]:
+            wyraz = wyraz[1:]
+            temp = temp[1:]
+            count = count + 1
+            if count == 7:
+                temp = 'papadam'
+                count = 0
+                wynik = wynik + 1
+        else:
+            wyraz = wyraz[1:]
+    print(wynik)
